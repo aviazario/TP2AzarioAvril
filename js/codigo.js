5b55ba85f41576fe311b7960c589e807e2eb6b5c
aviazario <136516741+aviazario@users.noreply.github.com>
2023-06-13 18:34:32 -0300

    Add files via upload

---
+++ js/codigo.js
@@ -0,0 +1,20 @@
+document.getElementById('menu').addEventListener('click', function () {
+
+    document.getElementById('navega').classList.toggle('mostrar');
+
+
+})
+
+
+
+
+lightbox.option({
+    'wrapAround': true,
+    'albumLabel': "Imagen %1 de %2",
+    'fadeDuration': 2000,
+    'maxWidth': 50,
+    'maxHeight': 50
+})
+
+
+
